7cd7e7d8fe10f9ee5d7fe05a1b2df25a4813968d
Lan Zukanović <lzukanovic@gmail.com>
2022-11-29 12:26:53 +0100

    bootstrap installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {AdminModule} from "./admin/admin.module";
 import {PublicModule} from "./public/public.module";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import {PublicModule} from "./public/public.module";
     BrowserModule,
     AppRoutingModule,
     AdminModule,
-    PublicModule
+    PublicModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
