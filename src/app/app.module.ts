0aacda2b194a79de73556194f6d7688bb6142df7
Lan Zukanović <lzukanovic@gmail.com>
2022-11-29 12:55:17 +0100

    fix dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {AdminModule} from "./admin/admin.module";
-import {PublicModule} from "./public/public.module";
+import { AdminModule } from "./admin/admin.module";
+import { PublicModule } from "./public/public.module";
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
